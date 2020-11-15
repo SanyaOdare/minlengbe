97256a60f390d657bdd0d646ca18cfdec7785363
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-11-15 01:08:36 +0100

    create components directory

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './SearchResults.css';
+
+class SearchResults extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        {/* <TrackList /> */}
+      </div>
+    )
+  }
+}
